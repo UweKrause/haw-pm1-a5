ed17e043ba855a722dcf736fb7dd91a9dcdc2834
Uwe Krause <Uwe.Krause@haw-hamburg.de>
2015-12-08 00:39:03 +0100

    Mastermindklasse nimmt Default-Code entgegen

--- mastermind_testcases_uwe.rb
+++ mastermind_testcases_uwe.rb
@@ -1,12 +1,43 @@
-require 'Mastermind'
+require 'mastermind_uwe'
 require 'test/unit'
 
 class MastermindTestcases < Test::Unit::TestCase
   def setup()
-    @mm = Mastermind.new()
+    @mm = Mastermind.new("1234")
+    @sample = @mm.sample
   end
-  
-  def test_keine_treffer
-    @mm.hi
+
+  def test_erwartung_erfuellt?
+
+    # gewonnen
+    assert_equal("XXXX", @mm.hits("1234", @sample)[2])
+
+    # keiner richtig
+    assert_equal("____", @mm.hits("9999", @sample)[2])
+
+    # paar schwarze, paar weisse, paar daneben
+    assert_equal("XO__", @mm.hits("1122", @sample)[2])
+    assert_equal("XX__", @mm.hits("2233", @sample)[2])
+    assert_equal("XX__", @mm.hits("2244", @sample)[2])
+    assert_equal("XO__", @mm.hits("2211", @sample)[2])
+    assert_equal("XX__", @mm.hits("1331", @sample)[2])
+    assert_equal("XX__", @mm.hits("4334", @sample)[2])
+    assert_equal("OOOO", @mm.hits("2143", @sample)[2])
+    assert_equal("XO__", @mm.hits("5814", @sample)[2])
+    assert_equal("XXOO", @mm.hits("3214", @sample)[2])
+    assert_equal("XXX_", @mm.hits("1231", @sample)[2])
+    assert_equal("XXX_", @mm.hits("1239", @sample)[2])
+    assert_equal("XXO_", @mm.hits("1293", @sample)[2])
+    assert_equal("X___", @mm.hits("1111", @sample)[2])
+    assert_equal("X___", @mm.hits("2222", "1234")[2])
+  end
+
+  def test_fehlerhafte_uebergabe
+    assert_raise RangeError do
+      @mm.hits("")
+    end
+    assert_raise ArgumentError do
+      @mm.hits()
+    end
   end
 end
--- mastermind_uwe.rb
+++ mastermind_uwe.rb
@@ -1,34 +1,41 @@
 # Mastermind Hauptklasse
 # Author:: Uwe Krause
 
-## Vorschauversion,
-## noch lange nicht fertig!!
-
 class Mastermind
+  # debug-option
   attr_reader :sample
-  def initialize(seed = nil)
+
+  # Stelt die Einstellungen der Solver-Klasse zur Verfuegung
+  attr_reader :felder, :range
+  def initialize(vorgabe = nil)
+    # Vorgabe als vierstelliger String erwartet!
+
+    ##
     # Spielregelvariablen hier:
+    ##
 
     @felder = 4
     @versuche = 10
     @range = (0..9)
 
-    @sample = create_rand(seed)
+    ##
+    # Instanzvariablen
+    ##
+    @sample = create_rand(@felder, vorgabe)
+
   end
 
-  # Gibt string zurueck
-  def create_rand(seed = nil)
+  # Gibt zufaelligen String zurueck
+  def create_rand(felder = @felder, vorgabe = nil)
 
-    seeds = {
-      1 => "1234",
-      2 => "3456"}
-
-    if (seed != nil) && seeds.has_key?(seed) && @felder == 4
-      seeds[seed]
+    if ((vorgabe != nil) && vorgabe.size == felder)
+      s = vorgabe
     else
-      a = @range.to_a.sample(@felder)
+      a = @range.to_a.sample(felder)
       s = a[0].to_s + a[1].to_s + a[2].to_s + a[3].to_s
     end
+
+    s
 
   end
 
@@ -36,33 +43,35 @@ class Mastermind
     raise RangeError unless tipp.is_a?(String)
     raise RangeError unless tipp.length == sample.length
 
-    # TODO: funktionierende Abfrage, ob nur Ziffern eingegeben wurden
-    #raise RangeError unless tipp =~ /[0-9]/ # ... filtert nicht korrekt!
-
-    # TODO: entweder keine Exceptions schmeissen, oder sie wenigstens anstaendig abfangen
-    # rescue ???
-
     schwarz = []
     weiss = []
     i = 0
 
     tipp.length.times do
 
+      # Das funktioniert auf diese Weise nur, wenn im Zufallswert keine Farben mehrfach vergeben sein duerfen!
+
       unless (schwarz.include?(tipp[i]))
+        #  wenn dieser Tipp bereits schwarz war, darf er auch nicht mehr als weiss gewertet werden
 
         if tipp[i] == sample[i]
+          # black hit
           schwarz << tipp[i]
-          weiss.delete(tipp[i])
-        elsif sample.include?(tipp[i])
 
+          # wurde bereits vorher nach diesem Tipp gefragt, waere er in weiss, er kann aber nicht gleichzeitig schwarz und weiss sein
+          # Solange im Zufallswert keine doppelten Werte erlaubt sind!!!
+          weiss.delete(tipp[i])
+
+        elsif sample.include?(tipp[i])
+          # white hit
           weiss << tipp[i] unless weiss.include?(tipp[i])
         end
       end
 
       i += 1
     end
-
-    ret = [schwarz.size, weiss.size, hits_to_s(schwarz.size, weiss.size)]
+    # [Anzahl schwarze Treffer, Anzahl weisse Treffer, Als String formartiert]
+    [schwarz.size, weiss.size, hits_to_s(schwarz.size, weiss.size)]
 
   end
 
@@ -73,4 +82,12 @@ class Mastermind
     string += "O" * white
     string += "_" * (felder - black - white)
   end
+
+  def to_s
+    @sample
+  end
 end
+
+#p Mastermind.new.hits("3523", "2532")
+#mm = Mastermind.new()
+#p mm.sample
