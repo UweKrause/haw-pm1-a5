0c088d6e890fe670738fff07b43a74b52dc82d06
lucasanders <lucas.anders@haw.hamburg.de>
2015-12-14 13:17:52 +0100

    Testfälle ergänzt und kommentiert

--- mastermind_test.rb
+++ mastermind_test.rb
@@ -6,11 +6,13 @@ require 'test/unit'
 #Author:: Lucas Anders
 class MastermindTest < Test::Unit::TestCase
   def setup
-    @mastermind = Mastermind.new()
+    @mastermind = Mastermind.new([1,2,3,4])
     @human = Mastermind_Human.new()
     @solver = Mastermind_Solver.new()
+
   end
 
+  #testet, ob die ungueltigen Tipps durchlaufen
   def test_guesses
     assert(@mastermind.check_attempt([1,2,3,4]))
     assert(@mastermind.check_attempt([4,2,7,5]))
@@ -22,9 +24,23 @@ class MastermindTest < Test::Unit::TestCase
     assert_equal(false, @mastermind.check_attempt([1,2,3]))
   end
 
+  #testet die Methode, die einen zufaelligen Tipp bestimmt
   def test_create_rand
     100.times do
-    assert(@mastermind.check_attempt(@mastermind.create_rand()))
+      assert(@mastermind.check_attempt(@mastermind.create_rand()))
     end
+  end
+
+  #testet die Methode, die die Hits bestimmt
+  def test_hits
+    assert_equal([4,0], @mastermind.hits([1,2,3,4]))
+    assert_equal([3,0], @mastermind.hits([1,2,3,6]))
+    assert_equal([2,0], @mastermind.hits([1,2,5,9]))
+    assert_equal([1,0], @mastermind.hits([1,6,7,8]))
+    assert_equal([2,2], @mastermind.hits([1,2,4,3]))
+    assert_equal([2,1], @mastermind.hits([1,2,4,8]))
+    assert_equal([1,3], @mastermind.hits([1,4,2,3]))
+    assert_equal([0,4], @mastermind.hits([4,3,2,1]))
+    assert_equal([0,2], @mastermind.hits([9,3,7,1]))
   end
 end
