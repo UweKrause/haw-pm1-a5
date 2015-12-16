0ea3e2757c78d8aa32b32c8a7c61c4703f75e27d
Uwe Krause <Uwe.Krause@haw-hamburg.de>
2015-12-16 02:12:50 +0100

    Berechnet die durchschnittliche Anzahl an Versuchen

---
+++ mastermind_solver_durchschnitt.rb
@@ -0,0 +1,44 @@
+require_relative 'mastermind_solver'
+
+# Diese  Klasse ueberprueft in einer angemessenen Anzahl von Versuchen, wie oft der Computer das Spiel innerhalb von 10 Versuchen loest
+# Author:: Uwe Krause
+def durchschnittliche_versuche(anzahl)
+
+  versuche_gesamt = []
+
+  anzahl.times do
+
+    @mastermind = Mastermind_Solver.new()
+
+    versuche = 0
+
+    while @mastermind.solved == false
+      versuche += 1
+
+      @mastermind.next_try()
+
+    end
+
+    versuche_gesamt << versuche if @mastermind.solved
+
+  end
+
+  versuche_gesamt
+
+end
+
+class Array
+  def summe
+    inject(0.0) { |result, el| result + el }
+  end
+
+  def durchschnitt
+    summe / size
+  end
+end
+
+anzahl = 100
+vs = durchschnittliche_versuche(anzahl)
+
+puts "Bei #{anzahl} Versuchen loest der Algorithmus das Spiel in #{vs.durchschnitt} Versuchen."
+puts "Hoechste Abzahl benoetigter Versuche: #{vs.max}"
